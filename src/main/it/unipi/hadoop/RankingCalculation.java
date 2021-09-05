bf775c4af734c9eb11afdb7bb6c3f5076380a3b0
gaetanoterra <73600008+gaetanoterra@users.noreply.github.com>
2021-09-05 18:37:57 +0200

    Update RankingCalculation.java

--- src/main/it/unipi/hadoop/RankingCalculation.java
+++ src/main/it/unipi/hadoop/RankingCalculation.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -14,11 +13,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.StringTokenizer;
 
 public class RankingCalculation {
